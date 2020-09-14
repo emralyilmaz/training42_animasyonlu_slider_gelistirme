5c7f317d5e766b6d9123dc89a0188e8c91d01199
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-14 20:30:38 +0300

    Slider Sınırlarını ve Yüksekliğini Belirleme

--- lib/paint.dart
+++ lib/paint.dart
@@ -20,7 +20,12 @@ class Painter extends CustomPainter {
           ..style = PaintingStyle.stroke
           ..strokeWidth = 2.5;
 
-  WaveLineDefinitions _useWaveLineDefinitions() {
+  WaveLineDefinitions _useWaveLineDefinitions(Size size) {
+    double maxWaveHeight = size.height * 0.8;
+    double minWaveHeight = size.height * 0.2;
+    double controlHeight =
+        size.height - minWaveHeight - (maxWaveHeight * dragYuzdelik);
+
     double bukulebilirlik = 25.0;
     double maxSliderDifference = 20.0;
     double slideDifference = (sliderPozisyon - _oncekiSliderPozisyon).abs();
@@ -35,15 +40,21 @@ class Painter extends CustomPainter {
     bool solYon = sliderPozisyon < _oncekiSliderPozisyon;
     bend = solYon ? -bend : bend;
 
-    double bendWidth =
-        40.0; // bend: bükmek anlamında bendwidth: bükülme genişliği demek
-    double bezierWidth = 40.0;
+    double bendWidth = 20.0 + 20 * dragYuzdelik;
+    // bend: bükmek anlamında bendwidth: bükülme genişliği demek
+    double bezierWidth = 20.0 + 20 * dragYuzdelik;
 
-    double startOfBend =
-        sliderPozisyon - bendWidth / 2; //bükmenin başlangıç noktası.
+    double startOfBend = sliderPozisyon - bendWidth / 2;
+    //bükmenin başlangıç noktası.
     double startOfBezierWidth = startOfBend - bezierWidth;
     double endOfBend = sliderPozisyon + bendWidth / 2;
     double endOfBezier = endOfBend + bezierWidth / 2;
+
+    startOfBend = (startOfBend < 0.0) ? 0.0 : startOfBend;
+    startOfBezierWidth = (startOfBezierWidth < 0.0) ? 0.0 : startOfBezierWidth;
+
+    endOfBend = (endOfBend >= size.width) ? size.width : endOfBend;
+    endOfBezier = (endOfBezier >= size.width) ? size.width : endOfBezier;
 
     double leftControlPoint1 = startOfBend;
     double leftControlPoint2 = startOfBend;
@@ -55,10 +66,9 @@ class Painter extends CustomPainter {
     rightControlPoint1 = rightControlPoint1 - bend;
     rightControlPoint2 = rightControlPoint2 + bend;
 
-    double controlHeight = 0;
     double centerPoint = sliderPozisyon;
 
-    centerPoint = centerPoint - bend;
+    centerPoint = (centerPoint > size.width) ? size.width : centerPoint;
 
     WaveLineDefinitions waveLine = WaveLineDefinitions(
         bendWidth: bendWidth,
@@ -104,7 +114,7 @@ class Painter extends CustomPainter {
   // }
 
   _paintWaveLine(Canvas canvas, Size size) {
-    WaveLineDefinitions waveLine = _useWaveLineDefinitions();
+    WaveLineDefinitions waveLine = _useWaveLineDefinitions(size);
 
     Path path = Path();
     path.moveTo(0.0, size.height);
