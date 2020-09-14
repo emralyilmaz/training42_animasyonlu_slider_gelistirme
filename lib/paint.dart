fe3d180c9c3f52533b2fcbd279754ad9217c5a5d
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-14 16:04:17 +0300

    Slider Çizgisini Oluşturma

--- lib/paint.dart
+++ lib/paint.dart
@@ -5,21 +5,35 @@ class Painter extends CustomPainter {
   final double dragYuzdelik;
   final Color renk;
   final Paint fillPainter;
+  final Paint wavePainter;
 
   Painter({this.sliderPozisyon, this.dragYuzdelik, this.renk})
       : fillPainter = Paint()
           ..color = renk
-          ..style = PaintingStyle.fill;
+          ..style = PaintingStyle.fill,
+        wavePainter = Paint()
+          ..color = renk
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 2.5;
+
   @override
   void paint(Canvas canvas, Size size) {
     _paintAnchors(canvas, size);
     // bu metodda çizim gerçekleştiriliyor.
+    _paintLine(canvas, size);
   }
 
   _paintAnchors(Canvas canvas, Size size) {
     // canvas çizim yapılacak aractır. Size ise içerisine yerleştirilecek widgetin boyutunu belirtir.
     canvas.drawCircle(Offset(0.0, size.height), 5.0, fillPainter);
     canvas.drawCircle(Offset(size.width, size.height), 5.0, fillPainter);
+  }
+
+  _paintLine(Canvas canvas, Size size) {
+    Path path = Path();
+    path.moveTo(0.0, size.height); // çizginin başlangıç noktası
+    path.lineTo(size.width, size.height); // çizginin bitiş noktası
+    canvas.drawPath(path, wavePainter);
   }
 
   @override
