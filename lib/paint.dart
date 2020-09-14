c70d3054f52aec49c63d6a082bd3854622c34389
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-14 17:54:14 +0300

    WaveLineDefinitions Sınıfını Oluşturma

--- lib/paint.dart
+++ lib/paint.dart
@@ -16,34 +16,7 @@ class Painter extends CustomPainter {
           ..style = PaintingStyle.stroke
           ..strokeWidth = 2.5;
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    _paintAnchors(canvas, size); // bu metodda çizim gerçekleştiriliyor.
-    //_paintLine(canvas, size);
-    //_paintBlock(canvas, size);
-    _paintWaveLine(canvas, size);
-  }
-
-  _paintAnchors(Canvas canvas, Size size) {
-    // canvas çizim yapılacak aractır. Size ise içerisine yerleştirilecek widgetin boyutunu belirtir.
-    canvas.drawCircle(Offset(0.0, size.height), 5.0, fillPainter);
-    canvas.drawCircle(Offset(size.width, size.height), 5.0, fillPainter);
-  }
-
-  _paintLine(Canvas canvas, Size size) {
-    Path path = Path();
-    path.moveTo(0.0, size.height); // çizginin başlangıç noktası
-    path.lineTo(size.width, size.height); // çizginin bitiş noktası
-    canvas.drawPath(path, wavePainter);
-  }
-
-  _paintBlock(Canvas canvas, Size size) {
-    Rect sliderRect =
-        Offset(sliderPozisyon, size.height - 7.5) & Size(5.0, 15.0);
-    canvas.drawRect(sliderRect, fillPainter);
-  }
-
-  _paintWaveLine(Canvas canvas, Size size) {
+  WaveLineDefinitions _useWaveLineDefinitions() {
     double bendWidth =
         40.0; // bend: bükmek anlamında bendwidth: bükülme genişliği demek
     double bezierWidth = 40.0;
@@ -62,13 +35,69 @@ class Painter extends CustomPainter {
     double controlHeight = 0;
     double centerPoint = sliderPozisyon;
 
+    WaveLineDefinitions waveLine = WaveLineDefinitions(
+        bendWidth: bendWidth,
+        bezierWidth: bezierWidth,
+        startOfBend: startOfBend,
+        startOfBezierWidth: startOfBezierWidth,
+        endOfBend: endOfBend,
+        endOfBezier: endOfBezier,
+        leftControlPoint1: leftControlPoint1,
+        leftControlPoint2: leftControlPoint2,
+        rightControlPoint1: rightControlPoint1,
+        rightControlPoint2: rightControlPoint2,
+        controlHeight: controlHeight,
+        centerPoint: centerPoint);
+    return waveLine;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _paintAnchors(canvas, size); // bu metodda çizim gerçekleştiriliyor.
+    //_paintLine(canvas, size);
+    //_paintBlock(canvas, size);
+    _paintWaveLine(canvas, size);
+  }
+
+  _paintAnchors(Canvas canvas, Size size) {
+    // canvas çizim yapılacak aractır. Size ise içerisine yerleştirilecek widgetin boyutunu belirtir.
+    canvas.drawCircle(Offset(0.0, size.height), 5.0, fillPainter);
+    canvas.drawCircle(Offset(size.width, size.height), 5.0, fillPainter);
+  }
+
+  // _paintLine(Canvas canvas, Size size) {
+  //   Path path = Path();
+  //   path.moveTo(0.0, size.height); // çizginin başlangıç noktası
+  //   path.lineTo(size.width, size.height); // çizginin bitiş noktası
+  //   canvas.drawPath(path, wavePainter);
+  // }
+
+  // _paintBlock(Canvas canvas, Size size) {
+  //   Rect sliderRect =
+  //       Offset(sliderPozisyon, size.height - 7.5) & Size(5.0, 15.0);
+  //   canvas.drawRect(sliderRect, fillPainter);
+  // }
+
+  _paintWaveLine(Canvas canvas, Size size) {
+    WaveLineDefinitions waveLine = _useWaveLineDefinitions();
+
     Path path = Path();
     path.moveTo(0.0, size.height);
-    path.lineTo(startOfBezierWidth, size.height);
-    path.cubicTo(leftControlPoint1, size.height, leftControlPoint2,
-        controlHeight, centerPoint, controlHeight);
-    path.cubicTo(rightControlPoint1, controlHeight, rightControlPoint2,
-        size.height, endOfBezier, size.height);
+    path.lineTo(waveLine.startOfBezierWidth, size.height);
+    path.cubicTo(
+        waveLine.leftControlPoint1,
+        size.height,
+        waveLine.leftControlPoint2,
+        waveLine.controlHeight,
+        waveLine.centerPoint,
+        waveLine.controlHeight);
+    path.cubicTo(
+        waveLine.rightControlPoint1,
+        waveLine.controlHeight,
+        waveLine.rightControlPoint2,
+        size.height,
+        waveLine.endOfBezier,
+        size.height);
     path.lineTo(size.width, size.height);
     canvas.drawPath(path, wavePainter);
   }
@@ -78,4 +107,36 @@ class Painter extends CustomPainter {
     // bu metodda paintler arası versiyonlar inceleniyor. yani önceki versiyonları görebiliyoruz.
     return true;
   }
+}
+
+class WaveLineDefinitions {
+  double bendWidth;
+  double bezierWidth;
+
+  double startOfBend;
+  double startOfBezierWidth;
+  double endOfBend;
+  double endOfBezier;
+
+  double leftControlPoint1;
+  double leftControlPoint2;
+  double rightControlPoint1;
+  double rightControlPoint2;
+
+  double controlHeight;
+  double centerPoint;
+
+  WaveLineDefinitions(
+      {this.bendWidth,
+      this.bezierWidth,
+      this.startOfBend,
+      this.startOfBezierWidth,
+      this.endOfBend,
+      this.endOfBezier,
+      this.leftControlPoint1,
+      this.leftControlPoint2,
+      this.rightControlPoint1,
+      this.rightControlPoint2,
+      this.controlHeight,
+      this.centerPoint});
 }
